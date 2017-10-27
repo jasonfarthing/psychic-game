a328e5d24ebc3608e66bd6707725dcaee646d45b
jasonfarthing <jasonfarthingisnow@gmail.com>
2017-10-27 10:10:14 -0700

    add jQ and edits that dont work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,66 @@
-// generate a random letter
-// have the user pick a letter - on key up show result
-//compare user guess to computer guess
-//if not ===, user guess again
-//show letters that user has guessed
-//if wrong letter, subtract from available guesses
-//user keeps guessing up to 10x to try to win
+/*
+Psuedo Code:
 
-//add 1 to wins if user correct guess within 10 trys
-//add 1 to losses if user not correct within 10 trys
-// total 
+1. generate a random letter
+2. have the user pick a letter on keyboard
+3. on key up action, add letter to 'yourGuesses' and
+compare user guess to computer guess
+4. if User Guess does not ===, Computer guess, the user will guess again.
+and 1 Available guess will be removed from total, log letters that user has guessed, when user
+guesses 10 times without success, add 1 to the 'Losses' counter.
+5. If User guess is == to Computer Guess, add 1 to 'Wins' counter.
+6. Game is reset every time a 'Win" or a 'Loss" is logged.
 
+
+*/
 
 var userWins = 0;
 var userLoss = 0;
 var userGuessLeft = 10;
-var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+    "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var compGuess = letter[Math.floor(Math.random() * letter.length)];
 console.log(compGuess);
 
+
+
+// var userGuess = (userGuess).document.onkeyup();
+
 var userGuess = prompt("pick a letter");
 
+document.getElementById("yourGuesses").innerHTML = userGuess;
+document.getElementById("losses").innerHTML = userLoss++;
+document.getElementById("guessesLeft").innerHTML = userGuessLeft--;
 
-if (userGuess == compGuess) {
-    alert("You got it!");
-    (userWins++);
-}
-else if (userGuess !== compGuess) {
-    // alert("Nope, my letter was " + compGuess + " Try Again.");
-    document.getElementById("yourGuesses").innerHTML = userGuess;
-    document.getElementById("losses").innerHTML = userLoss++;
-    document.getElementById("guessesLeft").innerHTML = userGuessLeft--;
-
-}
+//I need a way to store the userGuess and show it in the input box for userGuesses
+//I need a 'for' loop somewhere to cycle through the guesses
+$(document).ready(function () {
 
 
 
+    document.onkeypress = function (event) {
+        if (userGuess == compGuess) {
+            alert("You got it!");
+            userWins++;
+        } else {
+            userGuessLeft--;
+
+        }
+        if (userGuessLeft = 0) {
+            userLoss++;
+        }
+
+        // alert("Nope, my letter was " + compGuess + " Try Again.");
+
+
+
+        function gameReset() {
+            userGuessesLeft = 10;
+            userGuess;
+        }
+
+
+
+    });
